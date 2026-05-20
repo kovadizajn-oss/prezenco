6cbf2216f896a3e712cbed3cc1f04fd9fca42423
Jobbicus <info.jobbicus@gmail.com>
2026-05-20 09:21:01 +0200

    Allow forgot and reset password pages without auth

--- middleware.ts
+++ middleware.ts
@@ -46,7 +46,9 @@ export async function middleware(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/signup") &&
     !request.nextUrl.pathname.startsWith("/invite") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/auth") &&
+    !request.nextUrl.pathname.startsWith("/forgot-password") &&
+    !request.nextUrl.pathname.startsWith("/reset-password")
   ) {
     const url = request.nextUrl.clone();
     url.pathname = "/login";
